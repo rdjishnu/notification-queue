5edbafe526db15a604766872a08bbe34db4593c6
R D JISHNU PRIYAN <jishnupriyan2007@gmail.com>
2026-06-02 21:25:07 +0530

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/jishnu/notification_queue/NotificationQueueApplication.java
@@ -0,0 +1,13 @@
+package com.jishnu.notification_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jishnu/notification_queue/NotificationQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jishnu.notification_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
