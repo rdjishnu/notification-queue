0f4b530f392848f13a1c2a3550cc33cc68eed89a
R D JISHNU PRIYAN <jishnupriyan2007@gmail.com>
2026-06-05 12:20:41 +0530

    feat: implement QueueManager with add, process, peek and size

--- src/main/java/com/jishnu/notification_queue/QueueManager.java
+++ src/main/java/com/jishnu/notification_queue/QueueManager.java
@@ -1,7 +1,30 @@
 package com.jishnu.notification_queue;
+
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueManager {
-    private Queue<Notification> queue;
-    
+    private Queue<Notification> queue = new LinkedList<>();
+
+    public void addNotification(Notification notification) {
+        queue.add(notification);
+        System.out.println("Added: " + notification.getId());
+    }
+
+    public Notification processNext() {
+        Notification notification = queue.poll();
+        if (notification != null) {
+            notification.setStatus("SENT");
+            System.out.println("Processed: " + notification.getId());
+        }
+        return notification;
+    }
+
+    public Notification peekNext() {
+        return queue.peek();
+    }
+
+    public int getQueueSize() {
+        return queue.size();
+    }
 }
