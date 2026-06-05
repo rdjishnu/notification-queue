a87a0e7c42e3c354f5afd3fde46ca2cfe32cf8cc
R D JISHNU PRIYAN <jishnupriyan2007@gmail.com>
2026-06-05 12:27:47 +0530

    queue manager is working with fifo logic

--- src/main/java/com/jishnu/notification_queue/NotificationQueueApplication.java
+++ src/main/java/com/jishnu/notification_queue/NotificationQueueApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotificationQueueApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationQueueApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationQueueApplication.class, args);
 
+        QueueManager manager = new QueueManager();
+
+        manager.addNotification(new Notification("1", "rdjp@gmail.com", "Welcome!"));
+        manager.addNotification(new Notification("2", "ronny@gmail.com", "Your order is ready"));
+        manager.addNotification(new Notification("3", "lh44@gmail.com", "Reset your password"));
+
+        System.out.println("Queue size: " + manager.getQueueSize());
+        System.out.println("Next up: " + manager.peekNext().getRecipient());
+
+        manager.processNext();
+        manager.processNext();
+
+        System.out.println("Queue size after processing: " + manager.getQueueSize());
+    }
 }
